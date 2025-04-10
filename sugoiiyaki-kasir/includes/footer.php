a57a1c048c776d23fccd0a9d7fc34d54e05d8670
Windy Destiny Tarmidi <132053657+windydestiny2@users.noreply.github.com>
2025-04-11 01:28:15 +0700

    Add sugoiiyaki-kasir/includes/footer.php 1744309694852

---
+++ sugoiiyaki-kasir/includes/footer.php
@@ -0,0 +1,9 @@
+</div><!-- end container -->
+
+    <footer class="bg-donker text-white p-4 mt-8">
+        <div class="container mx-auto text-center">
+            © 2025 Sugoiiyaki. All rights reserved.
+        </div>
+    </footer>
+</body>
+</html>
